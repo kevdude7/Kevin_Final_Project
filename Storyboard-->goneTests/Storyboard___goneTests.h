c1aa26b8b8f9101fef5b5264cd718fefc6d8dec1
iD Student <Student@idtech-id05.princeton.edu>
2012-07-04 18:49:36 -0700

    lols

---
+++ Storyboard-->goneTests/Storyboard___goneTests.h
@@ -0,0 +1,13 @@
+//
+//  Storyboard___goneTests.h
+//  Storyboard-->goneTests
+//
+//  Created by iD Student on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Storyboard___goneTests : SenTestCase
+
+@end
---
+++ Storyboard-->goneTests/Storyboard___goneTests.m
@@ -0,0 +1,32 @@
+//
+//  Storyboard___goneTests.m
+//  Storyboard-->goneTests
+//
+//  Created by iD Student on 7/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "Storyboard___goneTests.h"
+
+@implementation Storyboard___goneTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Storyboard-->goneTests");
+}
+
+@end
